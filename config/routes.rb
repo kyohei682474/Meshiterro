36663f1ddcb074de4ddd596e78d4765ebe3eca07
kyohei682474 <118451830+kyohei682474@users.noreply.github.com>
2023-02-09 12:10:36 +0000

    meshi1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
+  
+  get 'homes/about'  => 'homes#about', as: 'about'
+  
 end
