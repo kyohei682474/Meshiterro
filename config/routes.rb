c75325f9816cc1b7d4b8c22ccb533900422f3ddc
kyohei682474 <118451830+kyohei682474@users.noreply.github.com>
2023-02-11 06:11:46 +0000

    meshi2

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
-  
+  resources :post_images, only: [:new, :create, :index, :show]
+ 
   get 'homes/about'  => 'homes#about', as: 'about'
   
 end
